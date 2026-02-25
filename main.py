fbfded6a1e64449b6a22c94ef7d4cb2adfa2121a
张浩 <386708307@qq.com>
2026-02-25 15:23:16 +0800

    bookstore

--- main.py
+++ main.py
@@ -17,3 +17,26 @@ app.include_router(router_example.router)
 @app.get("/")
 async def read_root():
     return {"message": "Hello World"}
+
+
+@app.get("/books/{book_id}")  # http://127.0.0.1:8000/book/11
+async def read_book(book_id: int):
+    return {"book_id": book_id, "title": "The Greate Gatsby", "author": "F.Scott Fitzgerald"}
+
+
+@app.get("/authors/{author_id}")  # http://127.0.0.1:8000/authors/1
+async def read_author(author_id: int):
+    return {
+        "author_id": author_id,
+        "name": "Ernest Hemingway"
+    }
+
+
+@app.get("/books")  # http://127.0.0.1:8000/books?year=2025
+async def read_books(year: int = None):
+    if year:
+        return {
+            "year": year,
+            "books": ["Book 1", "Book 2"]
+        }
+    return {"books": ["All Books"]}
